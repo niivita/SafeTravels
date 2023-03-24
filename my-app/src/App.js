8e382b76cffeea45f1ba3cd7c4ac5452b7cc851c
Quyen Huynh <samhuynh@Sams-MacBook-Pro-94.local>
2023-03-24 15:55:39 -0400

    debug

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import FillForm from './pages/FillForm';
+import GroupPage from './pages/GroupPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" exact element={<Home />} />
           <Route path="/FillForm" element={<FillForm />} />
+          <Route path="/GroupPage" element={<GroupPage />} />
         </Routes>
       </Router>
     </main>

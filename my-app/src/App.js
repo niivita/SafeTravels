815181d4100c5b725c49c798bd83176b85ebbe79
Tim Rupnarain <timrup@Tims-MacBook-Pro-3.local>
2023-04-21 21:24:23 -0400

    chatcomponents

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from "./pages/Home";
 import FillForm from './pages/FillForm';
 import GroupPage from './pages/GroupPage';
+import ChatPage from './pages/ChatPage';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact element={<Home />} />
           <Route path="/FillForm" element={<FillForm />} />
           <Route path="/GroupPage" element={<GroupPage /> }/> 
+          <Route path="/Chat" element={<ChatPage /> }/> 
         </Routes>
       </Router>
     </main>
---
+++ my-app/src/pages/ChatPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ChatPage.css'
+
+function ChatPage() {
+    return (
+      <iframe src="http://localhost:3001"></iframe>
+    );
+  }
+export default ChatPage;
